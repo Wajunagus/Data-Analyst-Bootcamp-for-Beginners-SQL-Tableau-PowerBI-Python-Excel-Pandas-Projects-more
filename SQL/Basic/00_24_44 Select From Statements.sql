72b238ee7160990c1e9e8e7e2699fb8b21f7650a
Wajunagus <Wajunagus@users.noreply.github.com>
2025-09-25 11:59:19 +0300

    mod(sql)
    SQL\Basic\00_24_44 Select From Statements.sql

--- SQL/Basic/00_24_44 Select From Statements.sql
+++ SQL/Basic/00_24_44 Select From Statements.sql
@@ -1,8 +1,6 @@
 /*                                                                                                             Thu, Sep 18, '25
 Select + From statements
-
 Top, Distinct, Count, As, Max, Min, Avg
-
 -------------------------------------------------------------------------------------------------------------------------------
 Choosing what database & what table to hit off of
 */
@@ -20,9 +18,7 @@ From MuyaTheAnalyst.dbo.EmployeeSalary
 
 /*-----------------------------------------------------------------------------------------------------------------------------
 Distinct
-
 When we use distinct, we're actually saying that we want the unique values in a specific column.
-
 Everything should be returned, so all 9 rows should be returned, & thats coz every single 1 of these are unique.
 */
 Select Distinct(EmployeeID)
@@ -34,9 +30,7 @@ From MuyaTheAnalyst..EmployeeDemographics
 
 /*-----------------------------------------------------------------------------------------------------------------------------
 Count
-
 Count is going to show us all the Non Null values in a column.
-        
 All that it's going to give us is a count of 9, coz we have 9 last names.
 If for whatever reason somebody's last name was left out, & that was null,
 then it would have returned maybe 8 or 7 depending on how many were actually in there.
@@ -45,13 +39,13 @@ If an entire column was null, it would be returned 0.
 Select Count(LastName)
 From MuyaTheAnalyst..EmployeeDemographics
 
-/*
+/*-----------------------------------------------------------------------------------------------------------------------------
 As
 */
 Select COUNT(LastName) As LastNameCount -- Giving it a column name
 From MuyaTheAnalyst..EmployeeDemographics
 
-/*
+/*-----------------------------------------------------------------------------------------------------------------------------
 Max, Min, & Avg
 
 @ our salary table we have salaries that would be useful for this information.
